86aee0bc1ea09f4833d3558d5010ad3f4f7160a1
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-19 23:10:06 +0300

    Initial commit

---
+++ src/main/java/entities/Category.java
@@ -0,0 +1,15 @@
+package entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Category {
+    private long id;
+    private String name;
+}
