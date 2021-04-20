d6f8f8aa01ddada1a6412791a1d0d3f1802d1067
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-20 16:02:59 +0300

    Update

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import entities.Category;
 import entities.Pet;
+import entities.User;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
@@ -18,7 +19,7 @@ public class ApiTests {
     public void test_add_pet(){
         Category cats = new Category(123, "Cats");
 
-        System.out.println("I preparing test data...");
+        System.out.println("Preparing test data...");
 
         Pet petToAdd = Pet.builder()
                 .id(new Random().nextInt(3))
@@ -31,98 +32,75 @@ public class ApiTests {
 
         System.out.println("Body to send: " + new Gson().toJson(petToAdd));
 
+        Response addingPetResponse = given()
+                .baseUri(BASE_URL)
+                .basePath("/pet")
+                .contentType(ContentType.JSON)
+                .body(petToAdd)
+                .when()
+                .post();
+
+        //Pet addedPetResponse = addingPetResponse.as(Pet.class);
+
+        System.out.println("Preparing for GET request by ID...");
+
         long id = petToAdd.getId();
 
-        Response addingPetResponse = given()
+        Response getPetInfo = given()
                 .baseUri(BASE_URL)
                 .pathParam("petId", id)
                 .when()
                 .get("/pet/{petId}");
 
-        System.out.println("Response GET: " + addingPetResponse.asString());
+        System.out.println("Response GET: " + getPetInfo.asString());
 
-        Pet gotPetResponse = addingPetResponse.as(Pet.class);
-        Assert.assertEquals("Wrong name", petToAdd.getName(), gotPetResponse.getName());
+        Pet gotPetResponse = getPetInfo.as(Pet.class);
+        Assert.assertEquals("Name does not match", petToAdd.getName(), gotPetResponse.getName());
     }
 
 
+    @Test
+    public void userRegister(){
+        System.out.println("Preparing test data...");
 
+        User userToReg = User.builder()
+                .id(new Random().nextInt(3))
+                .username("Juli145")
+                .firstName("Yulia")
+                .lastName("Frankova")
+                .email("test@gmail.com")
+                .password("12345")
+                .phone("0937881359")
+                .userStatus(1)
+                .build();
 
-//    @Test
-//    public void addNewPetToTheStore() {
-//        Category catCategory = new Category(131231231, "Cats");
-//
-//        System.out.println("I preparing test data...");
-//        Pet petToAdd = Pet.builder()
-//                .id(new Random().nextInt(3))
-//                .category(catCategory)
-//                .name("Jozzy")
-//                .photoUrls(Collections.singletonList("urls"))
-//                .tags(null)
-//                .status("available")
-//                .build();
-//        System.out.println("Body to send: " + new Gson().toJson(petToAdd));
-//
-//        Response addingPetResponse = given()
-//                .baseUri(BASE_URL)
-//                .basePath("/pet")
-//                .contentType(ContentType.JSON)
-//                .body(petToAdd)
-//                .when()
-//                .post();
-//
-//        System.out.println("Response: " + addingPetResponse.asString());
-//
-//        Pet addedPetResponse = addingPetResponse.as(Pet.class);
-//
-//        String expectedStatus = petToAdd.getStatus();
-//        String actualStatus = addedPetResponse.getStatus();
-//
-//        Assert.assertEquals("Wrong status code", 200, addingPetResponse.getStatusCode());
-//        Assert.assertEquals("Wrong status", expectedStatus, actualStatus);
-//    }
+        System.out.println("Body to send: " + new Gson().toJson(userToReg));
 
-   // @Test
-//    public void changeInfoAboutPet() {
-//        Category catCategory = new Category(131231231, "Cats");
-//
-//        System.out.println("I preparing test data...");
-//        Pet petToAdd = Pet.builder()
-//                .id(new Random().nextInt(3))
-//                .category(catCategory)
-//                .name("Jozzy")
-//                .photoUrls(Collections.singletonList("urls"))
-//                .tags(null)
-//                .status("available")
-//                .build();
-//        System.out.println("Body to send: " + new Gson().toJson(petToAdd));
-//
-//        Response addingPetResponse = given()
-//                .baseUri(BASE_URL)
-//                .basePath("/pet")
-//                .contentType(ContentType.JSON)
-//                .body(petToAdd)
-//                .when()
-//                .post();
-//
-//        System.out.println("Response: " + addingPetResponse.asString());
-//
-//        Pet addedPetResponse = addingPetResponse.as(Pet.class);
-//        long petId = addedPetResponse.getId();
-//
-//        petToAdd.setName("Qwerty");
-//        Pet changedPet = petToAdd;
-//
-//        Response changePetIdResponse = given()
-//                .baseUri(BASE_URL)
-//                .basePath("/pet")
-//                .contentType(ContentType.JSON)
-//                .body(changedPet)
-//                .when()
-//                .put();
-//
-//        Pet changedPetFromResponse = changePetIdResponse.as(Pet.class);
-//
-//        Assert.assertEquals("Wrong new name of pet", petToAdd.getName(), changedPetFromResponse.getName());
-//    }
+        Response addUser = given()
+                .baseUri(BASE_URL)
+                .basePath("/user")
+                .contentType(ContentType.JSON)
+                .body(userToReg)
+                .when()
+                .post();
+
+        System.out.println("Response: " + addUser.asString());
+
+      //  User addedUserResponse = addUser.as(User.class);
+
+        Assert.assertEquals("User not created", 200, addUser.getStatusCode());
+
+        System.out.println("Preparing for GET request by username...");
+
+        Response getEmail = given()
+                .baseUri(BASE_URL)
+                .pathParam("username", "Juli145")
+                .when()
+                .get("/user/{username}");
+
+        System.out.println("Response GET: " + getEmail.asString());
+
+        User gotUserResponse = getEmail.as(User.class);
+        Assert.assertEquals("Email does not match", userToReg.getEmail(), gotUserResponse.getEmail());
+    }
 }
